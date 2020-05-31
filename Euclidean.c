7a77987d6b3fa4af48d585f55d431bb744e7f04e
sreyassc <62113743+sreyassc@users.noreply.github.com>
2020-05-31 17:52:36 +0530

    Create Euclidean.c

---
+++ Euclidean.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+struct distance{
+          int d1,d2;
+           }p; 
+void main()
+   {
+      float sum; 
+      printf("Enter the distance between first two points\n");
+      scanf("%d",&p. d1); 
+      printf("Enter the distance between other two points\n");
+      scanf("%d",&p. d2); 
+      sum=p.d1+p.d2;
+      printf("The total distance between the points%f",sum);
+    }
